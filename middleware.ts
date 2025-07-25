a3222eee16b92e8c8d4347df84b09d38242b8426
visvoker <s2089533@gmail.com>
2025-07-25 18:18:19 +0800

    feat(middleware): 設計路由保護與登入導向邏輯
    - 未登入者訪問私頁會導向 /sign-in
    - 已登入者訪問 /sign-in 或 /sign-up 將導向 select-org 或所屬 organization
    - 若登入但尚未選取 orgId，則統一導向 /select-org

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,30 @@
+import { NextResponse } from "next/server";
+
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/"]);
 
 export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
+  const { userId, orgId } = await auth();
+
+  // 非公開頁面未登入 → 跳轉 sign-in
+  if (!userId && !isPublicRoute(req)) {
     await auth.protect();
+  }
+
+  // 已登入但在公開頁面 → 自動導向創建團隊頁面
+  if (userId && isPublicRoute(req)) {
+    const path = orgId ? `/organization/${orgId}` : "/select-org";
+    const orgSelection = new URL(path, req.url);
+
+    return NextResponse.redirect(orgSelection);
+  }
+
+  // 已登入但尚未選取 orgId→ 自動導向創建團隊頁面
+  if (userId && !orgId && req.nextUrl.pathname !== "/select-org") {
+    const orgSelection = new URL("/select-org", req.url);
+
+    return NextResponse.redirect(orgSelection);
   }
 });
 
