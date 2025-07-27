96e1f673e6bbb691527052b367a942803f8d23b0
visvoker <s2089533@gmail.com>
2025-07-27 15:10:41 +0800

    feat(sidebar): 完成 sidebar 基本 UI 結構

--- app/(platform)/(dashboard)/organization/layout.tsx
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -1,5 +1,4 @@
-import { Sidebar } from "lucide-react";
-import { ReactNode } from "react";
+import Sidebar from "../_components/sidebar";
 
 export default function OrganizationLayout({
   children,
@@ -10,7 +9,7 @@ export default function OrganizationLayout({
     <main className="pt-20 md:pt-24 px-4 max-w-6xl 2xl:max-w-screen-xl mx-auto">
       <div className="flex gap-x-7">
         <div className="w-64 shrink-0 hidden md:block bg-amber-300">
-          {/* {Sidebar} */}
+          <Sidebar />
         </div>
         {children}
       </div>
