eabab342628499e611bef3d6197a60a2b6782e2d
visvoker <s2089533@gmail.com>
2025-08-04 14:52:58 +0800

    feat(form): 新增 FormInput 元件

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -2,9 +2,9 @@
 
 import { createBoard } from "@/actions/create-board";
 
-import { FormInput } from "./form-input";
-import { FormButton } from "./form-button";
 import { useAction } from "@/hooks/use-action";
+import FormSubmit from "@/components/form/form-submit";
+import { FormInput } from "@/components/form/form-input";
 
 export default function Form() {
   const { execute, fieldErrors } = useAction(createBoard, {
@@ -19,14 +19,16 @@ export default function Form() {
   const onSubmit = (formData: FormData) => {
     const title = formData.get("title") as string;
 
+    console.log(title);
+
     execute({ title });
   };
   return (
     <form action={onSubmit}>
       <div className="flex flex-col space-y-2">
-        <FormInput errors={fieldErrors} />
+        <FormInput label="Board Title" id="title" errors={fieldErrors} />
       </div>
-      <FormButton />
+      <FormSubmit>Save</FormSubmit>
     </form>
   );
 }
