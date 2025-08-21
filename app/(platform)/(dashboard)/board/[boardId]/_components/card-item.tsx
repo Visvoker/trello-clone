9dbb75350743032a02272534ae6d6873608a200b
visvoker <s2089533@gmail.com>
2025-08-21 12:02:22 +0800

    feat(dnd): 整合卡片與列表的拖放功能

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -1,17 +1,25 @@
 import { Card } from "@/lib/generated/prisma";
+import { Draggable } from "@hello-pangea/dnd";
 
 type CardItemProps = {
   data: Card;
   index: number;
 };
 
-export default function CardItem({ data, index }: CardItemProps) {
+export function CardItem({ data, index }: CardItemProps) {
   return (
-    <div
-      role="button"
-      className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
-    >
-      {data.title}
-    </div>
+    <Draggable draggableId={data.id} index={index}>
+      {(provided) => (
+        <div
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+          role="button"
+          className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm "
+        >
+          {data.title}
+        </div>
+      )}
+    </Draggable>
   );
 }
--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { useRef, useState } from "react";
+import { Draggable, Droppable } from "@hello-pangea/dnd";
 
+import { cn } from "@/lib/utils";
 import { ListWithCards } from "@/type";
 
-import ListHeader from "./list-header";
+import { CardItem } from "./card-item";
 import { CardForm } from "./card-form";
-import { cn } from "@/lib/utils";
-import CardItem from "./card-item";
+import ListHeader from "./list-header";
 
 type ListItemProps = {
   data: ListWithCards;
@@ -31,27 +32,45 @@ export default function ListItem({ data, index }: ListItemProps) {
   };
 
   return (
-    <li className="shrink-0 h-full w-[272px] select-non!e">
-      <div className="w-full rounded-md bg-[#f1f2f4] shadow-md pb-2">
-        <ListHeader data={data} onAddCard={disableEditing} />
-        <ol
-          className={cn(
-            "mx-1 px-1 py-0.5 flex flex-col gap-y-2",
-            data.cards.length > 0 ? "mt-2" : "mt-0"
-          )}
+    <Draggable draggableId={data.id} index={index}>
+      {(provided) => (
+        <li
+          {...provided.draggableProps}
+          ref={provided.innerRef}
+          className="shrink-0 h-full w-[272px] select-non!e"
         >
-          {data.cards.map((card, index) => (
-            <CardItem index={index} key={card.id} data={card} />
-          ))}
-        </ol>
-        <CardForm
-          listId={data.id}
-          ref={textareaRef}
-          isEditing={isEditing}
-          enableEditing={enableEditing}
-          disableEditing={disableEditing}
-        />
-      </div>
-    </li>
+          <div
+            {...provided.dragHandleProps}
+            className="w-full rounded-md bg-[#f1f2f4] shadow-md pb-2"
+          >
+            <ListHeader onAddCard={enableEditing} data={data} />{" "}
+            <Droppable droppableId={data.id} type="card">
+              {(provided) => (
+                <ol
+                  ref={provided.innerRef}
+                  {...provided.droppableProps}
+                  className={cn(
+                    "mx-1 px-1 py-0.5 flex flex-col gap-y-2",
+                    data.cards.length > 0 ? "mt-2" : "mt-0"
+                  )}
+                >
+                  {data.cards.map((card, index) => (
+                    <CardItem index={index} key={card.id} data={card} />
+                  ))}
+                  {provided.placeholder}
+                </ol>
+              )}
+            </Droppable>
+            <CardForm
+              listId={data.id}
+              ref={textareaRef}
+              isEditing={isEditing}
+              enableEditing={enableEditing}
+              disableEditing={disableEditing}
+            />
+          </div>
+        </li>
+      )}
+    </Draggable>
   );
 }
