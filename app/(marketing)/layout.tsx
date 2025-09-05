4c72c4d7bb88bcb164b55d0ecb70b388ff3c4166
visvoker <s2089533@gmail.com>
2025-09-05 12:07:37 +0800

    fix(layout): 移除 MarketingLayout 的背景顏色

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -8,7 +8,7 @@ export default function MarketingLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="h-full bg-slate-500">
+    <div className="h-full">
       <Navbar />
       <main className="pt-40 pb-20 bg-slate-100">{children}</main>
       <Footer />
